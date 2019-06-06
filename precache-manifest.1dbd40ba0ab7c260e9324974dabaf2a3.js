1b37cb4cd959ae6da302c0d2aef4b719eb82de33
Martin Petr <martinpetr@localhost.localdomain>
2019-06-06 23:25:28 +0200

    Updates

---
+++ precache-manifest.1dbd40ba0ab7c260e9324974dabaf2a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9efc0fb312aace4c99b5e74dac0d2ef",
+    "url": "/web-animation/index.html"
+  },
+  {
+    "revision": "a2ac1b56eddf8005abe0",
+    "url": "/web-animation/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/web-animation/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a2ac1b56eddf8005abe0",
+    "url": "/web-animation/static/js/main.f4d45297.chunk.js"
+  },
+  {
+    "revision": "114008f4fc0df641c8b5",
+    "url": "/web-animation/static/js/runtime~main.622743ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-animation/static/media/logo.5d5d9eef.svg"
+  }
+]);
